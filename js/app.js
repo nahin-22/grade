5428a6f046832d5a26eaad7978e673327483b96c
nahin-22 <143338368+nahin-22@users.noreply.github.com>
2023-11-08 01:01:20 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+let num = window.prompt("Enter Your Number:");
+ 
+ if ( (num % 2) == 0){
+    document.write( num + " is Even Number");
+}
+ else {
+    document.write( num + " is Odd Number");
+}
